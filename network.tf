c6a34cb63e8d8690494e3b8754718839729a4166
unknown <Administrator@DARWEESH.S>
2025-06-08 14:14:18 -0700

    Fix Terraform formatting

--- network.tf
+++ network.tf
@@ -1,19 +1,19 @@
 # This Terraform configuration sets up a VPC and a subnet in AWS.
 resource "aws_vpc" "darweesh_vpc" {
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
-    tags = {
-        Name = "darweesh-vpc"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "darweesh-vpc"
+  }
 }
 
 resource "aws_subnet" "darweesh_subnet" {
-    vpc_id                  = aws_vpc.darweesh_vpc.id
-    cidr_block              = "10.0.1.0/24"
-    availability_zone       = "us-east-2a"
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "darweesh-subnet"
-    }
+  vpc_id                  = aws_vpc.darweesh_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "darweesh-subnet"
+  }
 }
