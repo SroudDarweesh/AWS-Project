6bad9c437a8e188cbc99712bac48b20406a7e17a
unknown <Administrator@DARWEESH.S>
2025-06-08 14:42:53 -0700

    add route table and internet gateway

--- network.tf
+++ network.tf
@@ -17,3 +17,47 @@ resource "aws_subnet" "darweesh_subnet" {
     Name = "darweesh-subnet"
   }
 }
+resource "aws_internet_gateway" "darweesh_igw" {
+  vpc_id = aws_vpc.darweesh_vpc.id
+  tags = {
+    Name = "darweesh-igw"
+  }
+}
+resource "aws_route_table" "darweesh_route_table" {
+  vpc_id = aws_vpc.darweesh_vpc.id
+  tags = {
+    Name = "darweesh-route-table"
+  }
+}
+resource "aws_security_group" "darweesh_sg" {
+  name        = "darweesh-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.darweesh_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "darweesh-sg"
+  }
+}
