5e75c04bbc6c0fcc1bf3bfe19c5d2cac2a40d02e
unknown <Administrator@DARWEESH.S>
2025-06-08 18:11:02 -0700

    10 VM instances

---
+++ instances.tf
@@ -0,0 +1,13 @@
+# This Terraform configuration creates 10 EC2 instances in AWS with a specific security group for RDP access.
+resource "aws_instance" "ec2_instances" {
+    count         = 10
+    ami           = "ami-070f90bab72874c6f"
+    instance_type = "t3.medium"
+    subnet_id     = aws_subnet.darweesh_subnet.id
+    vpc_security_group_ids = [aws_security_group.darweesh_sg.id]
+    associate_public_ip_address = true
+    key_name = "windows-key"
+    tags = {
+        Name = "darweesh-${count.index + 1}"
+    }
+}
