1394cba04c3bdb1baeb0d06430f208ef8827b02c
unknown <Administrator@DARWEESH.S>
2025-06-10 00:15:45 -0700

    redeploy

--- providers.tf
+++ providers.tf
@@ -10,13 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
-
-
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "env:/terraform.tfstate"
-    region         = "us-east-2"
-    use_lock_table = true  # ✅ replaces `dynamodb_table`
-  }
-}
